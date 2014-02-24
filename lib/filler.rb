cc08d0c0d58d76788e3a78e7b7056447093cdd3f
stevenspiel <stevenspiel@gmail.com>
2014-02-24 16:21:55 -0600

    add initial arrays of words with basic methods for sampling random words

--- lib/filler.rb
+++ lib/filler.rb
@@ -1,5 +1,57 @@
+require '../words/words.rb'
+
+
 class Filler
-  def self.hi
-    puts "Hello world!"
+  def self.random_noun
+    NOUNS.sample
   end
+
+  def self.random_verb
+    VERBS.sample
+  end
+
+  def self.random_adjective
+    ADJECTIVES.sample
+  end
+
+  def self.random_proposition
+    PREPOSITIONS.sample
+  end
+
+  def self.random_adverb
+    ADVERBS.sample
+  end
+
+  def self.random_pronoun
+    PRONOUNS.sample
+  end
+
+  def self.random_conjunctions
+    CONJUNCTIONS.sample
+  end
+
+  def self.proper_noun
+    PROPER_NOUNS.sample
+  end
+
+  class Name
+    def self.first_name
+      FIRST_NAMES.sample
+    end
+
+    def self.last_name
+      LAST_NAMES.sample
+    end
+
+    def self.name
+      "#{self.first_name} #{self.last_name}"
+    end
+  end
+
+  class Dictionary
+    def self.random_word
+    end
+  end
+
 end
+
