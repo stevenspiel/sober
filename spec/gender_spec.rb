79165ade8919b161767b68140e5fce64b3e13c51
Steven Spiel <stevenspiel@gmail.com>
2014-03-10 08:29:49 -0500

    add tests for getting a smart gender

--- spec/gender_spec.rb
+++ spec/gender_spec.rb
@@ -24,4 +24,16 @@ describe TextEng::Gender do
     end
   end
 
+  describe '#smart' do
+    it "should produce a male gender when passed a male name" do
+      male_name = TextEng::Name.first_name(:male)
+      expect(TextEng::Gender.smart(male_name))
+    end
+
+    it "should produce a female gender when passed a female name" do
+      female_name = TextEng::Name.first_name(:female)
+      expect(TextEng::Gender.smart(female_name))
+    end
+  end
+
 end
